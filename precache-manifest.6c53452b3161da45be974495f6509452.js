d2bf7f75125f8e08d0c35584b32c9dc6edc672eb
Andrey Malinyak <andrey.malyniak@gmail.com>
2019-07-18 12:01:24 +0300

    map

---
+++ precache-manifest.6c53452b3161da45be974495f6509452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd15cb4729d6436cdbef71f2e9df650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c0ba005055ff098480",
+    "url": "/static/css/main.78cd3138.chunk.css"
+  },
+  {
+    "revision": "a1082d4f442cb749614c",
+    "url": "/static/js/2.73bb5046.chunk.js"
+  },
+  {
+    "revision": "d5c0ba005055ff098480",
+    "url": "/static/js/main.30daff68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
